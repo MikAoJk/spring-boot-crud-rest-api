88fe150b5ffe6836fcc151c6712648242f7d31de
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-06-04 12:49:16 +0200

    use @ServiceConnection

--- src/test/kotlin/io/github/mikaojk/springbootcrudrestapi/SpringBootCrudRestApiApplicationTests.kt
+++ src/test/kotlin/io/github/mikaojk/springbootcrudrestapi/SpringBootCrudRestApiApplicationTests.kt
@@ -4,6 +4,7 @@ import io.github.mikaojk.springbootcrudrestapi.user.UserRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -15,13 +16,9 @@ class SpringBootCrudRestApiApplicationTests {
     lateinit var userRepository: UserRepository
 
     @Container
-    var postgresql: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:14-alpine").apply {
-        withCommand("postgres", "-c", "wal_level=logical")
-        start()
-        System.setProperty("spring.datasource.url", "$jdbcUrl&reWriteBatchedInserts=true")
-        System.setProperty("spring.datasource.username", username)
-        System.setProperty("spring.datasource.password", password)
-    }
+    @ServiceConnection
+    var postgresql: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:14-alpine")
+
 
     @Test
     fun contextLoads() {
