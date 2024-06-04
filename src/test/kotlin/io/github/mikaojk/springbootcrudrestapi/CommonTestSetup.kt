c47243c903a6d3f9323a315e39813e987a01c237
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-06-04 10:04:01 +0200

    Update CommonTestSetup.kt

--- src/test/kotlin/io/github/mikaojk/springbootcrudrestapi/CommonTestSetup.kt
+++ src/test/kotlin/io/github/mikaojk/springbootcrudrestapi/CommonTestSetup.kt
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.junit.jupiter.Container
 import kotlin.concurrent.thread
 
 @Container
