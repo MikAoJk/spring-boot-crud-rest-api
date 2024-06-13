e13ccbf80f71532565bfd205e754cb435d2caaee
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-06-13 08:06:48 +0200

    Update SpringBootCrudRestApiApplicationTests.kt

--- src/test/kotlin/io/github/mikaojk/springbootcrudrestapi/SpringBootCrudRestApiApplicationTests.kt
+++ src/test/kotlin/io/github/mikaojk/springbootcrudrestapi/SpringBootCrudRestApiApplicationTests.kt
@@ -4,7 +4,6 @@ import io.github.mikaojk.springbootcrudrestapi.user.UserRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
@@ -16,8 +15,13 @@ class SpringBootCrudRestApiApplicationTests {
     lateinit var userRepository: UserRepository
 
     @Container
-    @ServiceConnection
-    var postgresql: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:14-alpine")
+    var postgresql: PostgreSQLContainer<*> = PostgreSQLContainer("postgres:14-alpine").apply {
+                    withCommand("postgres", "-c", "wal_level=logical")
+                    start()
+                    System.setProperty("spring.datasource.url", "$jdbcUrl&reWriteBatchedInserts=true")
+                    System.setProperty("spring.datasource.username", username)
+                    System.setProperty("spring.datasource.password", password)
+    }
 
 
     @Test
